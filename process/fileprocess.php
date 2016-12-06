2b689fa40a2272fd843f472250d36c42821ee4ea
sanzillion <sanzillion@users.noreply.github.com>
2016-12-06 22:29:09 +0800

    Test update commit
    Minimal Changes

--- process/fileprocess.php
+++ process/fileprocess.php
@@ -37,69 +37,49 @@ if(isset($_POST['sub'])){
 }
 
 if(isset($_POST['submit'])){
-// 	if(is_uploaded_file($_FILES['userfile']['tmp_name']) && 
-// 		is_uploaded_file($_FILES['yrs']['tmp_name'])){
-// 		if($_FILES['userfile']['type'] != "text/plain" && 
-// 			$_FILES['yrs']['type'] != "text/plain"){
-// 			echo "Invalid Filetype";
-// 		}
-// 		elseif($_FILES['userfile']['size'] > 5000 &&
-// 			$_FILES['yrs']['size'] > 5000){
-// 			echo "File too large";
-// 		}
-// 		elseif($_FILES['userfile']['size'] < 50 &&
-// 			$_FILES['yrs']['size'] < 50){
-// 			echo "File too small";
-// 		}
-// 		elseif($_FILES['userfile']['error'] > 0 &&
-// 			$_FILES['yrs']['error'] > 0){
-// 			echo "Invalid File/ No file";
-// 		}
-// 		else{
+	if(is_uploaded_file($_FILES['userfile']['tmp_name']) && 
+		is_uploaded_file($_FILES['yrs']['tmp_name'])){
+		if($_FILES['userfile']['type'] != "text/plain" && 
+			$_FILES['yrs']['type'] != "text/plain"){
+			echo "Invalid Filetype";
+		}
+		elseif($_FILES['userfile']['size'] > 5000 &&
+			$_FILES['yrs']['size'] > 5000){
+			echo "File too large";
+		}
+		elseif($_FILES['userfile']['size'] < 50 &&
+			$_FILES['yrs']['size'] < 50){
+			echo "File too small";
+		}
+		elseif($_FILES['userfile']['error'] > 0 &&
+			$_FILES['yrs']['error'] > 0){
+			echo "Invalid File/ No file";
+		}
+		else{
 			
-// 			$name = $_FILES['userfile']['name'].'<br>';
-// 			echo "Name: ".'<br>';
-// 			echo $_FILES['userfile']['type'].'<br>';
-// 			echo $_FILES['userfile']['size'].'<br>';
-// 			echo $_FILES['userfile']['tmp_name'].'<br>';
-// 			echo $_FILES['userfile']['error'].'<br>';
-// 			echo "Yrs: ".'<br>';
-// 			echo $_FILES['yrs']['type'].'<br>';
-// 			echo $_FILES['yrs']['size'].'<br>';
-// 			echo $_FILES['yrs']['tmp_name'].'<br>';
-// 			echo $_FILES['yrs']['error'].'<br>';
+			$name = $_FILES['userfile']['name'].'<br>';
 
-// 			// $uploaddir = '../sad/files/';
-// 			// $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-// 			// echo $uploadfile;
-// 			// if(move_uploaded_file($_FILES['userfile']['tmp_name'],$uploadfile)){
-// 			// 	echo "good!";
-// 			// }
-// 			// else{
-// 			// 	echo "not good!";
-// 			// }
+			$string = file_get_contents($_FILES['userfile']['tmp_name'], "r");
+			$string2 = file_get_contents($_FILES['yrs']['tmp_name'], "r");
+			$names = explode("\n", $string);
+			$yrs = explode("\n", $string2);
 
-// 			$string = file_get_contents($_FILES['userfile']['tmp_name'], "r");
-// 			$string2 = file_get_contents($_FILES['yrs']['tmp_name'], "r");
-// 			$names = explode("\n", $string);
-// 			$yrs = explode("\n", $string2);
+			$arraycount = count($names);
+			$arraycount2 = count($yrs);
+			if($arraycount == $arraycount2){
 
-// 			$arraycount = count($names);
-// 			$arraycount2 = count($yrs);
-// 			if($arraycount == $arraycount2){
+				$arraycount -=1;
 
-// 				$arraycount -=1;
+				for($i = 0; $i < $arraycount; $i++){
+				// echo $names[$i].'<br>';
+		    	$query = $db->prepare("INSERT INTO test SET 
+								name = ?, yrs = ?");
+				$query->bindParam(1,$names[$i]);
+				$query->bindParam(2,$yrs[$i]);
+				$query->execute();
 
-// 				for($i = 0; $i < $arraycount; $i++){
-// 				// echo $names[$i].'<br>';
-// 		    	$query = $db->prepare("INSERT INTO test SET 
-// 								name = ?, yrs = ?");
-// 				$query->bindParam(1,$names[$i]);
-// 				$query->bindParam(2,$yrs[$i]);
-// 				$query->execute();
-
-// 				}
-// 			}
+				}
+			}
 			
 
  ?>
